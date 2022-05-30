6bb06d5ff6ad15394090c35e2380beeab7cd5725
Janshen Vernon <janshen.vbb@gmail.com>
2022-05-30 12:43:11 -0300

    created database and table schemas

--- src/database/schemas.sql
+++ src/database/schemas.sql
@@ -1,25 +1,179 @@
 create database pokedexdb;
 
-create table raw_whole_pokedex(
+create table raw_pokedex(
     id serial primary key,
+    name varchar(120) not null,
     pokemonUrl text not null
 );
 
-create table whole_pokedex(
+create table national_pokedex(
     id serial primary key,
-    abilities json [] not null,
-    base_xp int not null,
-    forms array not null,
-    game_indices json [] not null,
+    abilities text not null,
+    base_experience int not null,
+    forms text not null,
+    game_indices text not null,
     height int not null,
-    held_items json [] not null,
-    id int not null,
+    held_items text not null,
+    dex_id int not null,
     is_default boolean not null,
     location_area_encounters text not null,
-    moves json [] not null,
-    species json not null,
-    sprites json not null,
-    stats json [] not null,
-    types json [] not null,
+    moves text not null,
+    species text not null,
+    sprites text not null,
+    stats text not null,
+    types text not null,
     weight int not null
 );
+
+create table kanto_pokedex(
+    id serial primary key,
+    abilities text not null,
+    base_experience int not null,
+    forms text not null,
+    game_indices text not null,
+    height int not null,
+    held_items text not null,
+    dex_id int not null,
+    is_default boolean not null,
+    location_area_encounters text not null,
+    moves text not null,
+    species text not null,
+    sprites text not null,
+    stats text not null,
+    types text not null,
+    weight int not null
+);
+
+create table johto_pokedex(
+    id serial primary key,
+    abilities text not null,
+    base_experience int not null,
+    forms text not null,
+    game_indices text not null,
+    height int not null,
+    held_items text not null,
+    dex_id int not null,
+    is_default boolean not null,
+    location_area_encounters text not null,
+    moves text not null,
+    species text not null,
+    sprites text not null,
+    stats text not null,
+    types text not null,
+    weight int not null
+);
+
+create table hoenn_pokedex(
+    id serial primary key,
+    abilities text not null,
+    base_experience int not null,
+    forms text not null,
+    game_indices text not null,
+    height int not null,
+    held_items text not null,
+    dex_id int not null,
+    is_default boolean not null,
+    location_area_encounters text not null,
+    moves text not null,
+    species text not null,
+    sprites text not null,
+    stats text not null,
+    types text not null,
+    weight int not null
+);
+
+create table sinnoh_pokedex(
+    id serial primary key,
+    abilities text not null,
+    base_experience int not null,
+    forms text not null,
+    game_indices text not null,
+    height int not null,
+    held_items text not null,
+    dex_id int not null,
+    is_default boolean not null,
+    location_area_encounters text not null,
+    moves text not null,
+    species text not null,
+    sprites text not null,
+    stats text not null,
+    types text not null,
+    weight int not null
+);
+
+create table unova_pokedex(
+    id serial primary key,
+    abilities text not null,
+    base_experience int not null,
+    forms text not null,
+    game_indices text not null,
+    height int not null,
+    held_items text not null,
+    dex_id int not null,
+    is_default boolean not null,
+    location_area_encounters text not null,
+    moves text not null,
+    species text not null,
+    sprites text not null,
+    stats text not null,
+    types text not null,
+    weight int not null
+);
+
+create table kalos_pokedex(
+    id serial primary key,
+    abilities text not null,
+    base_experience int not null,
+    forms text not null,
+    game_indices text not null,
+    height int not null,
+    held_items text not null,
+    dex_id int not null,
+    is_default boolean not null,
+    location_area_encounters text not null,
+    moves text not null,
+    species text not null,
+    sprites text not null,
+    stats text not null,
+    types text not null,
+    weight int not null
+);
+
+create table alola_pokedex(
+    id serial primary key,
+    abilities text not null,
+    base_experience int not null,
+    forms text not null,
+    game_indices text not null,
+    height int not null,
+    held_items text not null,
+    dex_id int not null,
+    is_default boolean not null,
+    location_area_encounters text not null,
+    moves text not null,
+    species text not null,
+    sprites text not null,
+    stats text not null,
+    types text not null,
+    weight int not null
+);
+
+create table galar_pokedex(
+    id serial primary key,
+    abilities text not null,
+    base_experience int not null,
+    forms text not null,
+    game_indices text not null,
+    height int not null,
+    held_items text not null,
+    dex_id int not null,
+    is_default boolean not null,
+    location_area_encounters text not null,
+    moves text not null,
+    species text not null,
+    sprites text not null,
+    stats text not null,
+    types text not null,
+    weight int not null
+);
+
