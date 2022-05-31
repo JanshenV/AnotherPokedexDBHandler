f2d1748db0f1fe6e84651562313ee3963219b903
Janshen Vernon <janshen.vbb@gmail.com>
2022-05-30 22:24:30 -0300

    changed the way the fetching data was being called

--- src/seeders/fetchingData.js
+++ src/seeders/fetchingData.js
@@ -1,6 +1,10 @@
 const knex = require('../database/connection');
 const fetch = require('node-fetch');
 
+//Util
+const filteringPokemonData = require('../util/dataFilter');
+const stringfyData = require('../util/stringfyData');
+
 async function fetchingRaw() {
     try {
         const existingRawPokedex = await knex('raw_pokedex')
@@ -12,96 +16,66 @@ async function fetchingRaw() {
 
         const { results } = await rawPokedexRequest.json();
 
-        for (let { name: rawName, url } of results) {
-            let rawPokemon = {
-                name: rawName,
-                pokemonurl: url
-            };
-            await knex('raw_pokedex').insert(rawPokemon);
-
-            const pokemonInfoRequest = await fetch(`${url}`);
-
-            if (!pokemonInfoRequest.ok) throw 'Error fetching individual pokemon';
-
-            const {
-                name, height, id: dex_id,
-                base_experience, is_default, location_area_encounters,
-                weight,
-                abilities, forms, game_indices, held_items,
-                moves, species, sprites,
-                stats, types
-            } = await pokemonInfoRequest.json();
-
-            const stringAbilities = JSON.stringify(abilities);
-            const stringForms = JSON.stringify(forms);
-            const stringGameIndices = JSON.stringify(game_indices);
-            const stringHeldItems = JSON.stringify(held_items);
-            const stringMoves = JSON.stringify(moves);
-            const stringSpecies = JSON.stringify(species);
-            const stringSprites = JSON.stringify(sprites);
-            const stringStats = JSON.stringify(stats);
-            const stringTypes = JSON.stringify(types);
-
-            const pokemonInfoData = {
-                name, height, dex_id,
-                base_experience, is_default, location_area_encounters,
-                weight,
-                abilities: stringAbilities,
-                forms: stringForms,
-                game_indices: stringGameIndices,
-                held_items: stringHeldItems,
-                moves: stringMoves,
-                species: stringSpecies,
-                sprites: stringSprites,
-                stats: stringStats,
-                types: stringTypes
-            };
-
-            await knex('national_pokedex').insert(pokemonInfoData);
-
-            if (dex_id <= 151) {
-                await knex('kanto_pokedex').insert(pokemonInfoData);
-            };
-
-            if (dex_id >= 152 && dex_id <= 251) {
-                await knex('johto_pokedex').insert(pokemonInfoData);
-            };
-
-            if (dex_id >= 252 && dex_id <= 386) {
-                await knex('hoenn_pokedex').insert(pokemonInfoData);
-            };
-
-            if (dex_id >= 387 && dex_id <= 493) {
-                await knex('sinnoh_pokedex').insert(pokemonInfoData);
-            };
-
-            if (dex_id >= 494 && dex_id <= 649) {
-                await knex('unova_pokedex').insert(pokemonInfoData);
-            };
-
-            if (dex_id >= 650 && dex_id <= 721) {
-                await knex('kalos_pokedex').insert(pokemonInfoData);
-            };
-
-            if (dex_id >= 722 && dex_id <= 809) {
-                await knex('alola_pokedex').insert(pokemonInfoData);
-            };
-
-            if (dex_id >= 810 && dex_id <= 898) {
-                await knex('alola_pokedex').insert(pokemonInfoData);
-            };
-
-            if (dex_id > 898) {
-                await knex('pokemon_variations').insert(pokemonInfoData);
-            };
+        for (let { name, url: pokemonurl } of results) {
+            await knex('raw_pokedex').insert({
+                name,
+                pokemonurl
+            });
         };
     } catch ({ message }) {
         return console.log({ message });
     };
 };
 
+async function fetchingFromRaw() {
+    try {
+        const rawPokedex = await knex('raw_pokedex')
+            .select('*');
+        if (!rawPokedex.length) throw 'Raw pokedex is empty';
 
+        for (let rawPokemon of rawPokedex) {
+            const { pokemonurl: url } = rawPokemon;
 
-module.exports = {
-    fetchingRaw,
+            const pokemonUrlRequest = await fetch(url);
+            if (!pokemonUrlRequest.ok) throw "Pokemon url request error";
+
+            const pokemonUrlResponse = await pokemonUrlRequest.json();
+            const {
+                newPokemonData, error: filteringError
+            } = await filteringPokemonData(pokemonUrlResponse);
+            if (filteringError) throw filteringError;
+
+            const {
+                stringfiedData, error: stringfyError
+            } = await stringfyData(newPokemonData);
+            if (stringfyError) throw stringfyError;
+
+            const formatedValueForDb = {
+                name: stringfiedData.name,
+                dexnr: stringfiedData.dexnr,
+                weight: stringfiedData.weight,
+                height: stringfiedData.height,
+                location_area_encounters: stringfiedData.location_area_encounters,
+                abilities: stringfiedData.abilities,
+                forms: stringfiedData.forms,
+                species: stringfiedData.species,
+                moves: stringfiedData.moves,
+                types: stringfiedData.types,
+                stats: stringfiedData.stats,
+                sprites: stringfiedData.sprites,
+            };
+
+            await knex('national_pokedex')
+                .insert(formatedValueForDb);
+        };
+    } catch ({ message }) {
+        return console.log({ message });
+    };
 };
+
+async function fetchingData() {
+    await fetchingRaw();
+    await fetchingFromRaw();
+};
+
+module.exports = fetchingData;
