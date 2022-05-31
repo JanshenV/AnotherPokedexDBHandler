3745004b2caa3ed0e6829d0a77cdf49d00dbadcc
Janshen Vernon <janshen.vbb@gmail.com>
2022-05-30 22:26:47 -0300

    removed previous controller

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ require('dotenv').config();
 const app = require('./server');
 
 //Seeders
-const { runningMigrations } = require('./seeders/migrations');
-const { fetchingRaw } = require('./seeders/fetchingData');
+const runningMigrations = require('./seeders/migrations');
+const fetchingData = require('./seeders/fetchingData');
 const callVariations = require('./seeders/pokemonVariations');
 
 async function seedersFirst() {
     try {
         await runningMigrations();
-        await fetchingRaw();
-        await callVariations();
+        await fetchingData();
+        // await callVariations();
         app.listen(process.env.SERVER_PORT);
     } catch ({ message }) {
         return console.log({ message });
--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,6 @@ const express = require('express');
 const routes = express();
 
 //Controllers
-const {
-    regionalPokedex
-} = require('./controllers/pokedex');
-
-
-routes.post('/regionalPokedex', regionalPokedex);
 
 
 module.exports = routes;
