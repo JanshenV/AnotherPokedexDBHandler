589ec03827e273e73e8cad7d3b7475f7a2a13cb4
Janshen Vernon <janshen.vbb@gmail.com>
2022-05-30 22:26:12 -0300

    removed the removed items from scemas from jsonParser

--- src/util/jsonParser.js
+++ src/util/jsonParser.js
@@ -1,15 +1,13 @@
 async function jsonParser(pokemonData) {
     try {
         const {
-            abilities, forms, game_indices,
-            held_items, moves, species,
+            abilities, forms,
+            moves, species,
             sprites, stats, types
         } = pokemonData;
 
         const jsonAbilities = await JSON.parse(abilities);
         const jsonForms = await JSON.parse(forms);
-        const jsonGameIndices = await JSON.parse(game_indices);
-        const jsonHeldItems = await JSON.parse(held_items);
         const jsonMoves = await JSON.parse(moves);
         const jsonSpecies = await JSON.parse(species);
         const jsonSprites = await JSON.parse(sprites);
@@ -20,8 +18,6 @@ async function jsonParser(pokemonData) {
             ...pokemonData,
             abilities: jsonAbilities,
             forms: jsonForms,
-            game_indices: jsonGameIndices,
-            held_items: jsonHeldItems,
             moves: jsonMoves,
             species: jsonSpecies,
             sprites: jsonSprites,
