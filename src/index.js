8d27a8c5a92199257508b922b25415f1fc041e5b
Janshen Vernon <janshen.vbb@gmail.com>
2022-06-01 18:49:17 -0300

    .

--- src/index.js
+++ src/index.js
@@ -9,11 +9,7 @@ async function seedersFirst() {
     try {
         await runningMigrations();
         await fetchingData();
-<<<<<<< HEAD
         app.listen(process.env.PORT || 3000);
-=======
-        app.listen(process.env.SERVER_PORT || 2700);
->>>>>>> 19290c6f486b5bd0ea5cc01d4925c77909870c26
     } catch ({ message }) {
         return console.log({ message });
     };
