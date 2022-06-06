5b1d42110e09b92869d9bae320ea034465996ce5
Janshen Vernon <janshen.vbb@gmail.com>
2022-06-06 20:00:18 -0300

    app listen outside function

--- src/index.js
+++ src/index.js
@@ -9,11 +9,12 @@ async function seedersFirst() {
     try {
         await runningMigrations();
         await fetchingData();
-        app.listen(process.env.SERVER_PORT);
     } catch ({ message }) {
         return console.log({ message });
     };
 };
 
 seedersFirst();
+
+app.listen(process.env.SERVER_PORT);
 
