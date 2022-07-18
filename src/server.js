406861b0e95b1e32599eb1fdd1f9e410e367c5a3
Janshen Vernon <janshen.vbb@gmail.com>
2022-07-18 18:11:57 -0300

    installed compression as dev dependecy

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
+const compression = require('compression');
 
 const app = express();
 
 app.use(express.json());
+app.use(compression())
 app.use(cors());
 app.use(routes);
 
